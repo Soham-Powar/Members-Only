9eb2b0da110f382b0625f9affd08bca882fb22bf
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-09-28 10:24:15 +0530

    feat: setup auth for the login route and fix typo

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,7 +1,17 @@
 const Router = require("express");
 const loginRouter = new Router();
+const passport = require("passport");
+
 const loginController = require("../controllers/loginController");
 
 loginRouter.get("/", loginController.loginGet);
+
+loginRouter.post(
+  "/",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login?error=Invalid credentials",
+  })
+);
 
 module.exports = loginRouter;
